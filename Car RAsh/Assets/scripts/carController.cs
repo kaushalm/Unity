0f6f34073e334ceaf59b0c91eba34cb4aab043cb
KM <kaushalmehra1987@gmail.com>
2017-11-09 23:28:24 +0530

    added car rash unity game project

---
+++ Car RAsh/Assets/scripts/carController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+    // Use this for initialization
+    public float carSpeed;
+    public float maxPos = 2.2f;
+    Vector3 position;
+    uiManager ui;
+
+
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    void Update()
+    {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        position.x = Mathf.Clamp(position.x, -2.2f, 2.2f);
+        transform.position = position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "EnemyCar")
+        {
+            Destroy(gameObject);
+            ui.GameOverActivated();
+        }
+    }
+
+ 
+
+}
---
+++ Car RAsh/Assets/scripts/trackMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackMove : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	Vector2 offset;
+
+
+	void start(){
+	}
+
+	void Update(){
+		offset = new Vector2(0, Time.time*speed);
+		GetComponent<Renderer>().material.mainTextureOffset= offset;
+	}
+}
